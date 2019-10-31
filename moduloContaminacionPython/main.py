70063208198524c5942df048b682b166ddc2146e
sergioeabarcaf <sergioaf1991@gmail.com>
2019-10-31 00:37:05 -0300

    programa base

---
+++ moduloContaminacionPython/main.py
@@ -0,0 +1,20 @@
+import sensorBME280
+import sensorCO2
+import sensorPM
+import converterTime
+import time
+
+# Funcion para obtener datos de medicion y enviarlos a firebase y CSV.
+def getData(dirFile, module, sessionNumber):
+    data = {'timestamp': converterTime.getTimestamp()}
+    data.update(sensorBME280.getTHPJSON())
+
+    print data
+
+while True:
+    // pedir datos de sensores y guardarlos en un json.
+    getData()
+    time.sleep(10)
+
+    // enviar los datos a firebase
+
