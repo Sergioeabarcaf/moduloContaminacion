82b71cd460d0a1880690b345a384ea00cc67b9df
sergioeabarcaf <sergioaf1991@gmail.com>
2019-11-06 20:38:49 -0300

    python - leer mensajes serial y pasar a string

--- moduloContaminacionPython/pivot.py
+++ moduloContaminacionPython/pivot.py
@@ -3,9 +3,13 @@ import serial
 ser = serial.Serial('/dev/ttyACM0')
 ser.baudrate = 9600
 
+encoding = 'utf-8'
+
 while(True):
     if(ser.is_open):
-        x = ser.read()
-        print(x)
+        x = ser.read_until()
+        x = x.decode(encoding)
+        if(len(x) < 20):
+            print(x)
     else:
         print("Comunicación Serial cerrada.")
